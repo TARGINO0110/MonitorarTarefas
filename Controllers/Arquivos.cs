0b307602bed18a8c9d75d488c33bd710daa71f06
TARGINO0110 <lucianoptargino1@outlook.com>
2020-02-15 23:25:13 -0300

    Add icons nos titulos+ mecanismo de arquivos + mascara input

---
+++ Controllers/Arquivos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Monitorar_Tarefas.Controllers
+{
+    public class Arquivos
+    {
+    }
+}
